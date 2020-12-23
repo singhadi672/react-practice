82fbbcda25da1d785a86d8fbb1113e5a8f059331
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2020-12-23 12:16:19 +0530

    Feat: added business and business list and styles

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from "../BusinessList/BusinessList"
+
+function App() {
+  return (
+    <div className="App">
+      <BusinessList/>
+    </div>
+  );
+}
+
+export default App;
